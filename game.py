263185b380f7e7536f14a820f0d7bc653d9a1071
Asher Matheson <ashrlmthsn@gmail.com>
2018-10-03 10:08:18 +1300

    Added "alive" attribute and checking for this

--- game.py
+++ game.py
@@ -14,18 +14,26 @@ class Entity():
     def __init__(self, img_path, enemy=True):
         self.image = pygame.image.load(img_path)
         self.hitbox = self.image.get_rect()
+        self.alive
 
         if enemy:
             Entity.enemies.append(self)
 
     def update(self):
-        self.xpos -= 0.1
+        if self.alive:
+            self.xpos -= 1
+            if random.random() > .999: #Chance to generate an emeny of same type as self
+                self.__class__() #No need to assign, handled by __init__
+
+        else:
+            Entity.enemies.remove(self)
+            if type(self).__name__ == "Bird":
+                Bird.birds.remove(self)
 
 
 class Player(Entity):
     def __init__(self):
         super().__init__('assets/dino.png', enemy=False)
-        self.alive = True
         self.xpos = 50
         self.ypos = 600 - self.image.get_size()[1]
         self.yvel = 0
@@ -37,8 +45,6 @@ class Player(Entity):
         self.score += 1
 
 class Cactus(Entity):
-
-    cacti = []
 
     def __init__(self):
         super().__init__('assets/cactus.png')
@@ -55,22 +61,20 @@ class Bird(Entity):
         self.ypos = (
             550 - self.image.get_size()[1] - (self.type * 50)
         )
-        Bird.birds.append(self)
-        if Bird.birds == []:
+
+        if Bird.birds != []:
             self.xpos = Bird.birds[-1].xpos + random.randint(150, 750)
         else:
             self.xpos = 1200
 
-player = Player()
-
 def play(networks):
 
+    scores = {}
     players = {}
     for network in networks:
         players[network] = Player()
 
     clock = pygame.time.Clock()
-    gameover = False
 
     while True:
 
@@ -81,14 +85,15 @@ def play(networks):
         screen.fill((255,255,255))
 
         for network in networks:
-            output = network.activate()
-            try:
-                if output.md=="jump":
-                    network.jump()
-                elif output.md=="duck":
-                    network.duck()
-            except:
-                pass
+            if network.alive:
+                output = network.activate()
+                try:
+                    if output.md=="jump":
+                        network.jump()
+                    elif output.md=="duck":
+                        network.duck()
+                except:
+                    pass
 
         for network in networks:
 
@@ -99,19 +104,19 @@ def play(networks):
                 enemy.update()
                 screen.blit(enemy, (enemy.xpos, enemy.ypos))
                 if curr_player.hitbox.colliderect(enemy.hitbox):
-                    gameover=True
+                    curr_player.alive = False
                     break
 
-            curr_player.update()
+            if not curr_player.alive: #Check if the current player is dead
+                scores[network] = curr_player.score #Add score to scores
+                del players[network] #Remove from list of avaliable players
 
-        if gameover:
-            break
+            curr_player.update()
 
         pygame.display.flip()
         clock.tick(30)
 
-    scores = {}
     for network in players:
-        scores[network] = players[network].score
+        scores[network] = players[network].score #Get score for each network
 
     return scores
